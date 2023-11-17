9aed58db2318009b4cc80a9595a6ba1c8f3ef5fc
Qlacreo <ale.oliva@duocuc.cl>
2023-11-17 20:45:56 -0300

    recuperacion contraseña

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
-
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) { 
+    
+  }
+
+  resetPassword(email: string): Promise<void> {
+    return this.afAuth.sendPasswordResetEmail(email);
+  }
 }
--- src/app/pages/recuperacion/recuperacion.page.ts
+++ src/app/pages/recuperacion/recuperacion.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
@@ -12,49 +13,45 @@ export class RecuperacionPage implements OnInit {
 
   formularioRecuperacion: FormGroup;
 
-  constructor(private router: Router, public fb: FormBuilder, public alertController: AlertController, private toastController: ToastController) { 
+  constructor(
+    private router: Router,
+    public fb: FormBuilder,
+    public alertController: AlertController,
+    private toastController: ToastController,
+    private afAuth: AngularFireAuth
+  ) {
     this.formularioRecuperacion = this.fb.group({
-      'correo': new FormControl("", Validators.required)
+      'correo': ['', [Validators.required, Validators.email]]
     });
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  async recuperar(){
-    var f = this.formularioRecuperacion.value;
+  async recuperar() {
+    const email = this.formularioRecuperacion.value.correo;
 
-    var usuarioString = localStorage.getItem('usuario');
-
-    if (usuarioString !== null) {
-      var usuario = JSON.parse(usuarioString);
-      if (usuario.correo == f.correo) {
-        console.log('Enviado');
-        localStorage.setItem('Enviado', 'true');
-        this.mensajeToast('Se envio un mensaje de recuperacion a su correo');
-        this.router.navigate(['login'])
-      } else {
-        const alert = await this.alertController.create({
-          header: 'Correo no valido',
-          message: 'El correo que escribio no es valido o no se encuentra registrado.',
-          buttons: ['Aceptar'],
-        });
-        await alert.present()
-        return;
-      }
-    } else {
-      // Manejo de caso cuando no se encuentra el valor en localStorage
+    try {
+      await this.afAuth.sendPasswordResetEmail(email);
+      console.log('Correo de restablecimiento enviado correctamente.');
+      this.mensajeToast('Se envió un mensaje de recuperación a su correo');
+      this.router.navigate(['login']);
+    } catch (error) {
+      console.error('Error al enviar el correo de restablecimiento:', error);
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Hubo un error al enviar el correo de restablecimiento. Por favor, inténtelo de nuevo.',
+        buttons: ['Aceptar'],
+      });
+      await alert.present();
     }
   }
 
-  async mensajeToast(mensaje:string) {
+  async mensajeToast(mensaje: string) {
     const toast = await this.toastController.create({
       message: mensaje,
       duration: 3000,
       position: 'top'
     });
-    toast.present()
+    toast.present();
   }
-
-
 }
